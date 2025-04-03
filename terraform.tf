3a52a92bc37b41572c1ebbc9a16a1f4070cd17cb
Darshit Mistry <darshitmistry2@gmail.com>
2025-04-03 16:01:36 +0530

    feat : remote backend add

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,11 @@ terraform {
       version = "5.91.0"
     }
   }
+
+  backend "s3" {
+    bucket = "state-bucket"
+    key    = "terraform.tfstate"
+    region  = "us-east-2"
+    dynamodb_endpoint = "state-table"
+  }
 }
